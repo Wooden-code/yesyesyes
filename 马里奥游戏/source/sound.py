10a4412b9c6078f4f5f63d7c2180910ff6c35d92
robot-2233 <2929608755@qq.com>
2022-03-18 15:39:12 +0800

    tyj

--- 马里奥游戏/source/sound.py
+++ 马里奥游戏/source/sound.py
@@ -3,7 +3,7 @@
 # @Author : 阿童木
 # @File : sound.py
 # @software: PyCharm
-import pygame,sys
+import pygame,sys,os
 
 class Music_BG:
     def __init__(self):
@@ -12,8 +12,7 @@ class Music_BG:
         self.game_info=None
     #背景音乐
     def update(self,surface,keys):#jinrule
-
-        pygame.mixer.music.load("resource\music\渔舟唱晚.ogg")
+        pygame.mixer.music.load(os.path.abspath("resource/music/渔舟唱晚.ogg"))#方法问题
         pygame.mixer.music.set_volume(1.0)#设置音量
         pygame.mixer.music.play(-1)#循环播放
 

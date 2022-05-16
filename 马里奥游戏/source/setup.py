7449c1a2ca37bb804896d1ef1730bf27770c0883
robot-2233 <2929608755@qq.com>
2022-05-16 21:49:10 +0800

    tyj

--- 马里奥游戏/source/setup.py
+++ 马里奥游戏/source/setup.py
@@ -11,18 +11,7 @@ from . import tools
 
 pygame.init()
 SCREEN=pygame.display.set_mode((C.SCREEN_W,C.SCREEN_H))#把屏幕做成一个常量
-# TODO:path
-GRAPHICS=tools.load_graphics(r'C:\Users\abc\Desktop\yesyesyes\马里奥游戏\resource\graphics')
 
-# try:
-#     abs = os.getcwd ()
-#     GRAPHICS=pygame.image.load(abs)
-# except FileNotFoundError:
-#     try:
-#         abs = os.getcwd()
-#         sds=abs.split(r'\\')
-#         sds.pop(-1)
-#         ewr=r'\\'.join(sds)+r'\\resource\\graphics'
-#         GRAPHICS = pygame.image.load(abs)
-#     except Exception as e:
-#         print(e)
+GRAPHICS=tools.load_graphics('resource/graphics')
+#abs = os.getcwd ()
+#GRAPHICS=tools.load_graphics(abs +'/resource/graphics')
